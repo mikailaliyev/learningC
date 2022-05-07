1f432bebdc6557fd9fb2f85f831870ec5b34cf34
mikailaliyev <mikail.aliyev@gmail.com>
2022-05-07 22:04:23 +0400

    added data types

---
+++ 2. DataTypes.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+//Main data types
+int main()
+{
+    int age = 50;
+    double gpa = 3.78;
+    char grade = 'A';
+    char phrase[] = "Great";// this is essentially equals to char phrase[6] = "Great";
+    return 0;
+}
