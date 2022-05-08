d34121bd57bccf007e3f610633ecd0e640928594
mikailaliyev <mikail.aliyev@gmail.com>
2022-05-08 16:25:22 +0400

    constants

---
+++ 6Constants.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+  //   const int NUM = 5;
+  //   printf("%d\n", NUM);
+  //   NUM = 0; //Will through an error
+  //   printf("%d", NUM);
+
+  // String also constant or immutable
+  printf("Hello\n");
+
+  // Numbers printed withour assigned to variables are
+  // also constants, so we have to manually edit them
+  printf("%d", 45);
+  return 0;
+}
