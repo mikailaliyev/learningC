cf49f8ff48772209dd705387558d43d724bdb69e
mikailaliyev <mikail.aliyev@gmail.com>
2022-05-08 15:22:56 +0400

    printf

--- 3Printf.c
+++ 3Printf.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 
 int main() {
-    printf("Hello");
+    // printf("Hello\" World");
+    int favoriteNumber = 40;
+    /* 
+    %c for char, %s for strings, %d for integers, %f for floats
+    All these format specifiers work with variables corresponding with corresponding data types
+    */
+    printf("My %cavorite %s is %d and %f", 'f', "number", favoriteNumber + 1, 55.445656);
+
     return 0;
 }
