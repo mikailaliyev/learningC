adff4eeb5a46cba906436403cdc9803fdc7aec0b
mikailaliyev <mikail.aliyev@gmail.com>
2022-05-10 23:52:18 +0400

    update

--- 2DataTypes.c
+++ 2DataTypes.c
@@ -5,6 +5,7 @@ int main()
     int age = 50;
     double gpa = 3.78;
     char grade = 'A';
+    
     char phrase[] = "Great";// this is essentially equals to char phrase[6] = "Great";
     return 0;
 }
