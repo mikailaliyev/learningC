9db24bf3cb4f330e9ade6299b8ae829ac1e57e7c
mikailaliyev <mikail.aliyev@gmail.com>
2022-05-11 23:31:02 +0400

    arrays

---
+++ 10Arrays.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    // int luckyNumbers[] = {1, 2, 3, 4, 5, 6};
+    // luckyNumbers[0] = 200;
+    // printf("%i\n", sizeof luckyNumbers / sizeof 1);
+    // printf("%i", luckyNumbers[0]);
+    int luckyNumbers[1];
+    luckyNumbers[0] = 100;
+    luckyNumbers[1] = 200;
+    printf("%i", luckyNumbers[1123]);
+    return 0;
+}
