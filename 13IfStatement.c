e3e897f9e04df4b1bba50668e27297e87e49cd59
mikailaliyev <mikail.aliyev@gmail.com>
2022-05-13 23:59:04 +0400

    update'

--- 13IfStatement.c
+++ 13IfStatement.c
@@ -15,5 +15,7 @@ int returnMaxNumber(int num1, int num2, int num3) {
   //   return num2;
   return num1 > num2 && num1 > num3   ? num1
          : num2 > num1 && num2 > num3 ? num2
-                                      : num3;
+                                      : num3; 
+                                      
+
 }
