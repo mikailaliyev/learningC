d5ea808b17b1ea0656b120928a4dc20074bbffad
mikailaliyev <mikail.aliyev@gmail.com>
2022-05-29 23:54:59 +0400

    exercises

---
+++ exercises/FahrenhiteToCelsius.c
@@ -0,0 +1,41 @@
+/*
+Author: Mikayil Aliyev
+Program: Fahrenhite to Celsius converter
+*/
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    char mode;
+    float fahrenhiteTemp;
+    float celsiusTemp;
+    float result;
+
+    printf("Choose mode!\nto Celsius press c\nto Fahrenhite press f\n");
+    scanf("%c", &mode);
+    while(mode != 'f' || mode != 'c') {
+
+
+    if (mode == 'c')
+    {
+        printf("You choosed \"to Celsius\" mode!");
+        printf("\nEnter Fahrenhite temperature you want to convert to Celsius!\n");
+        scanf("%f", &fahrenhiteTemp);
+        result = 5 * (fahrenhiteTemp - 32) / 9;
+        printf("The result is: %.2fF\n", result);
+
+    }
+    else if (mode == 'f')
+    {
+        printf("You choosed \"to Fahrenhite\" mode!");
+        printf("\nEnter Celsius temperature you want to convert to Fahrenhite!\n");
+        scanf("%f", &celsiusTemp);
+        result = (celsiusTemp * 9/5) + 32;
+        printf("The result is: %.2fC\n", result);
+    }
+
+    } 
+    system("pause");
+    return 0;
+}
