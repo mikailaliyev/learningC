588c6c5bc0f1d6276c1e9979fd43f6907577485a
mikailaliyev <mikail.aliyev@gmail.com>
2022-05-30 01:11:37 +0400

    'update'

--- exercises/FahrenhiteToCelsius.c
+++ exercises/FahrenhiteToCelsius.c
@@ -8,34 +8,54 @@ Program: Fahrenhite to Celsius converter
 int main()
 {
     char mode;
+    char proceed;
     float fahrenhiteTemp;
     float celsiusTemp;
     float result;
 
-    printf("Choose mode!\nto Celsius press c\nto Fahrenhite press f\n");
-    scanf("%c", &mode);
-    while(mode != 'f' || mode != 'c') {
-
-
-    if (mode == 'c')
+    while (proceed = 'y')
     {
-        printf("You choosed \"to Celsius\" mode!");
-        printf("\nEnter Fahrenhite temperature you want to convert to Celsius!\n");
-        scanf("%f", &fahrenhiteTemp);
-        result = 5 * (fahrenhiteTemp - 32) / 9;
-        printf("The result is: %.2fF\n", result);
+        printf("Choose mode!\nto Celsius press c\nto Fahrenhite press f\n");
+        scanf(" %c", &mode);
 
+        if (mode == 'c')
+        {
+            printf("You choosed \"to Celsius\" mode!");
+            printf("\nEnter Fahrenhite temperature you want to convert to Celsius!\n");
+            scanf("%f", &fahrenhiteTemp);
+            result = 5 * (fahrenhiteTemp - 32) / 9;
+            printf("The result is: %.2fF\n", result);
+            printf("Do you want to proceed?\n");
+            scanf(" %c", &proceed);
+            if (proceed != 'y')
+            {
+                return printf("End of the program!");
+            }
+        }
+        else if (mode == 'f')
+        {
+            printf("You choosed \"to Fahrenhite\" mode!");
+            printf("\nEnter Celsius temperature you want to convert to Fahrenhite!\n");
+            scanf("%f", &celsiusTemp);
+            result = (celsiusTemp * 9 / 5) + 32;
+            printf("The result is: %.2fC\n", result);
+            printf("Do you want to proceed?\n");
+            scanf(" %c", &proceed);
+            if (proceed != 'y')
+            {
+                return printf("End of the program!");
+            }
+        }
+        else
+        {
+            printf("You entered wrong character!\nDo you want to proceed?\n");
+            scanf(" %c", &proceed);
+            if (proceed != 'y')
+            {
+                return printf("End of the program!");
+            }
+        }
     }
-    else if (mode == 'f')
-    {
-        printf("You choosed \"to Fahrenhite\" mode!");
-        printf("\nEnter Celsius temperature you want to convert to Fahrenhite!\n");
-        scanf("%f", &celsiusTemp);
-        result = (celsiusTemp * 9/5) + 32;
-        printf("The result is: %.2fC\n", result);
-    }
-
-    } 
     system("pause");
     return 0;
 }
